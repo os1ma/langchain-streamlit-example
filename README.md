26bc2f01c5f83bc6579d8c48a32c060bc192d8f0
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-03 22:39:33 +0900

    Use vector store

--- README.md
+++ README.md
@@ -27,8 +27,20 @@ python -m venv .venv
 pip install -r requirements.txt
 ```
 
+文書をベクトル化してローカルに保存
+
+```console
+python create_index.py
+```
+
 アプリケーションの起動
 
 ```console
 streamlit run app.py
+```
+
+例えば以下のプロンプトで動作確認できます。
+
+```
+langchain-streamlit-example は何を import していますか？
 ```
--- app.py
+++ app.py
@@ -1,21 +1,44 @@
+import openai
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.agents import AgentType, initialize_agent
+from langchain.agents.agent_toolkits import VectorStoreInfo, VectorStoreToolkit
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import MessagesPlaceholder
+from langchain.vectorstores import Chroma
+
+from create_index import CHROMA_PERSIST_DIRECTORY
+
+openai.log = "debug"
 
 load_dotenv()
 
 
 def create_agent():
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
-    tools = load_tools(["terminal"])
+
+    # Setup VectorStore
+    embeddings = OpenAIEmbeddings()
+    db = Chroma(
+        embedding_function=embeddings, persist_directory=CHROMA_PERSIST_DIRECTORY
+    )
+    vectorstore_info = VectorStoreInfo(
+        vectorstore=db,
+        name="langchain-streamlit-example",
+        description="Source code of application named `langchain-streamlit-example`",
+    )
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    tools = toolkit.get_tools()
+
+    # Setup Memory
     agent_kwargs = {
         "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
     }
     memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
     return initialize_agent(
         tools,
         llm,
@@ -25,10 +48,10 @@ def create_agent():
     )
 
 
-st.title("ChatGPT-like clone")
-
 if "agent" not in st.session_state:
     st.session_state.agent = create_agent()
+
+st.title("ChatGPT-like clone")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
