840fd5a9fea8a6e6c3e0f8a3d5f101efd9aab2f5
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-03 22:08:08 +0900

    Add memory

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
-import langchain
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
-
-langchain.verbose = True
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
 
 load_dotenv()
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
-tools = load_tools(["terminal"])
-agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS)
+
+def create_agent():
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
+    tools = load_tools(["terminal"])
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    }
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+    return initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
+
 
 st.title("ChatGPT-like clone")
+
+if "agent" not in st.session_state:
+    st.session_state.agent = create_agent()
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -29,7 +44,7 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         st_callback = StreamlitCallbackHandler(st.container())
-        response = agent.run(prompt, callbacks=[st_callback])
+        response = st.session_state.agent.run(prompt, callbacks=[st_callback])
         st.markdown(response)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
